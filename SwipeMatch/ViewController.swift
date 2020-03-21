ec3c0f3f9bf5fcda53b27ecbf5f602a6aecb2921
AlekseyK <aleksey.kabishau@gmail.com>
2020-03-21 16:49:57 -0400

    Initial Commit

---
+++ SwipeMatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeMatch
+//
+//  Created by Aleksey on 0321..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
