a5f269d5a191326fc848c8d904e0043ebacef0da
AlekseyK <aleksey.kabishau@gmail.com>
2020-03-24 00:14:41 -0400

    feat: refactor card view using viewModel pattern

--- SwipeMatch/Screens/HomeVC.swift
+++ SwipeMatch/Screens/HomeVC.swift
@@ -14,9 +14,14 @@ class HomeVC: UIViewController {
     let cardDeckView = UIView()
     let buttonsStackView = HomeButtomControlsStackView()
     
-    let users = [
-        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "kelly1"),
-        User(name: "Jane", age: 18, profession: "Teacher", imageName: "jane1")
+//    let users = [
+//        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "kelly1"),
+//        User(name: "Jane", age: 18, profession: "Teacher", imageName: "jane1")
+//    ]
+    
+    let cardViewModels: [CardViewModel] = [
+        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "kelly1").toCardViewModel(),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "jane1").toCardViewModel()
     ]
 
     override func viewDidLoad() {
@@ -27,17 +32,11 @@ class HomeVC: UIViewController {
     
     private func setupDummyCards() {
         
-        users.forEach { (user) in
+        cardViewModels.forEach { (cardViewModel) in
             let cardView = CardView(frame: .zero)
-            cardView.imageView.image = UIImage(named: user.imageName)
-            
-//            cardView.infoLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
-            
-            let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
-            attributedText.append(NSAttributedString(string: " \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
-            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-            
-            cardView.infoLabel.attributedText = attributedText
+            cardView.imageView.image = UIImage(named: cardViewModel.imageName)
+            cardView.infoLabel.attributedText = cardViewModel.attributedString
+            cardView.infoLabel.textAlignment = cardViewModel.textAlignment
                 
             cardDeckView.addSubview(cardView)
             cardView.fillSuperview()
---
+++ SwipeMatch/ViewModels/CardViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewModel.swift
+//  SwipeMatch
+//
+//  Created by Aleksey on 0323..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // define properties that view will display/render out
+    
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
